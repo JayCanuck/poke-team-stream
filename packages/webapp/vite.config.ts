c02b53c3d8015d158e900f6a6a41aa06a859b6be
JayCanuck <robitaille88@gmail.com>
2023-12-04 12:54:07 -0800

    Placeholder favicon

--- packages/webapp/vite.config.ts
+++ packages/webapp/vite.config.ts
@@ -2,11 +2,17 @@
 import basicSsl from '@vitejs/plugin-basic-ssl';
 import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
+import vitePluginFaviconsInject from 'vite-plugin-favicons-inject';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [basicSsl(), react(), tsconfigPaths()],
+  plugins: [
+    basicSsl(),
+    react(),
+    tsconfigPaths(),
+    process.env.NODE_ENV == 'production' ? vitePluginFaviconsInject('./src/assets/icon.png') : false
+  ],
   build: {
     outDir: 'build',
     chunkSizeWarningLimit: 10_000
